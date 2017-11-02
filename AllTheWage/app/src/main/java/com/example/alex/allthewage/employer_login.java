0439f5b259b70d34534313e819a4843d9d60e471
Andres Ibarra <ai15c@my.fsu.edu>
2017-11-02 15:23:23 -0400

    Integrated Firebase Auth for both Employee and Employer
    Firebase Auth now works for both employers and employees
    will start to work on integrating Firebase Database soon.
    Also changed up the backend of the employer/employee login screens
    but they still work the same.

--- AllTheWage/app/src/main/java/com/example/alex/allthewage/employer_login.java
+++ AllTheWage/app/src/main/java/com/example/alex/allthewage/employer_login.java
@@ -3,16 +3,78 @@ package com.example.alex.allthewage;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.Button;
+import android.widget.*;
 
+//FIREBASE AUTH
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 /**
  * Created by Alex on 10/19/2017.
  */
 
-public class employer_login extends Activity{
+public class employer_login extends Activity implements
+        View.OnClickListener{
 
-    Button login;
+
+    private EditText EmailField;
+    private EditText PasswordField;
+
+    String email;
+    String password;
+
+    //DECLARING FIREBASE AUTH VARIABLE
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+
+        if(i == R.id.LoginButton1) {
+            email = EmailField.getText().toString();
+            password = PasswordField.getText().toString();
+
+            if(!email.isEmpty() && !password.isEmpty())
+            {
+                signIn(email, password);
+
+            }
+        }   //END OF IF LOGIN_BUTTON
+
+    }//END OF ONCLICK
+
+    private void signIn(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            System.out.println("COULD LOG IN");
+                            Intent employerHomeIntent = new Intent(employer_login.this, employer_home.class);
+                            startActivity(employerHomeIntent);
+
+
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            System.out.println("COULD NOT LOG IN");
+
+                        }
+                    }
+                });
+
+
+
+    }//END OF SIGN UP
+
 
 
     @Override
@@ -20,15 +82,22 @@ public class employer_login extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.employer_login);
 
-        login = (Button) findViewById(R.id.LoginButton1); //# 1 buttons are for employer
+        findViewById(R.id.LoginButton1).setOnClickListener(this);   //setting the click listner for Login Button
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent employerHomeIntent = new Intent(employer_login.this, employer_home.class);
-                startActivity(employerHomeIntent);
-            }
-        });
+        EmailField = (EditText) findViewById(R.id.employerEmailID);
+        PasswordField = (EditText) findViewById(R.id.employerPasswordID);
 
-    }
-}
+        mAuth = FirebaseAuth.getInstance();    //initializing Firebase Auth to be used to login
+
+        //DUMMY CALL TO SIGN IN BECAUSE FOR SOME REASON IT DOESN'T WORK ON THE FIRST TRY
+        mAuth.signInWithEmailAndPassword(" ", " ");
+
+
+
+
+    }//END OF ONCREATE
+
+
+
+}  //END OF FILE
+
