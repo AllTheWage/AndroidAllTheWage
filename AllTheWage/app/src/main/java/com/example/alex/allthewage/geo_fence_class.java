67e307c7ce851238cffbea42f31066cabba1da7a
schwalbevans <evanschwalb@gmail.com>
2017-12-05 01:26:51 -0500

    Started adding more geofence functionality

--- AllTheWage/app/src/main/java/com/example/alex/allthewage/geo_fence_class.java
+++ AllTheWage/app/src/main/java/com/example/alex/allthewage/geo_fence_class.java
@@ -1,19 +1,25 @@
 package com.example.alex.allthewage;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import static android.view.View.*;
+import static com.example.alex.allthewage.R.id.map;
 
 /**
  * Created by evan on 12/4/17.
@@ -27,9 +33,9 @@ public class geo_fence_class extends AppCompatActivity implements OnMapReadyCall
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.geo_fence_class);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        @SuppressLint("WrongViewCast") MapView mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
     }
 
     @Override
