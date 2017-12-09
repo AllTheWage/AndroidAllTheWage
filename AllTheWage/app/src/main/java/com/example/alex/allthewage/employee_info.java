fd6fe3a2219344e5b4feb7ddf282f04dce96c417
JJMingG <jamineguo@hotmail.com>
2017-12-08 19:24:48 -0500

    Implmented employee_info class
    Created a spinner that displays all empoyee info

--- AllTheWage/app/src/main/java/com/example/alex/allthewage/employee_info.java
+++ AllTheWage/app/src/main/java/com/example/alex/allthewage/employee_info.java
@@ -8,12 +8,53 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Alex on 11/1/2017.
+ * Modified by Jamine Guo on 12/8/2017.
+ *
+ * List of things to work on:
+ *      - Lock EditText lines
  */
 
 public class employee_info extends AppCompatActivity {
+
+    private List<String> IDs;
+    private List<String> names;
+    Spinner employeeSpinner;
+    ArrayAdapter employeeAdapter;
+    EditText employeeID;
+    EditText employeeName;
+    EditText employeeAge;
+    EditText employeeMail;
+    EditText employeeNum;
+
+    // variables for/from selected employee from spinner
+    private String selectedName;
+    private String selectedID;
+    private String selectedAge;
+    private String selectedMail;
+    private String selectedNum;
+
+    private FirebaseAuth mAuth= FirebaseAuth.getInstance();
+
+    DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("EMPLOYERS").child("Companies").child(mAuth.getUid()).child(globalVars.GlobalCompanyName);
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +66,83 @@ public class employee_info extends AppCompatActivity {
         // Make sure the toolbar exists in the activity and is not null
         setSupportActionBar(toolbar);
 
+        employeeID = (EditText) findViewById(R.id.employeeID);
+        employeeName = (EditText) findViewById(R.id.employeeName);
+        employeeAge = (EditText) findViewById(R.id.employeeAge);
+        employeeMail = (EditText) findViewById(R.id.employeeMail);
+        employeeNum = (EditText) findViewById(R.id.employeeNum);
+
+        // access employers database and implement spinner
+        ref.addValueEventListener(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(final DataSnapshot dataSnapshot) {
+
+                        // collectName((Map<String, Object>) dataSnapshot.getValue());
+                        names = new ArrayList<String>();
+                        names.add("None"); // Initial selection
+                        IDs = new ArrayList<String>();
+                        IDs.add("Employee ID"); // Initial selection
+
+                        // Iterate through employee IDs and retrieve names
+                        for(DataSnapshot employee : dataSnapshot.getChildren()){
+                            String name = employee.child("Name").getValue(String.class);
+                            String ID = employee.getKey();
+                            names.add(name);
+                            IDs.add(ID);
+                        }
+
+                        // Spinner implementation
+                        employeeSpinner = (Spinner) findViewById(R.id.employeeSpinner);
+                        employeeAdapter = new ArrayAdapter(employee_info.this, android.R.layout.simple_spinner_dropdown_item, names);
+                        employeeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        employeeSpinner.setAdapter(employeeAdapter);
+
+                        // Spinner selection
+                        employeeSpinner.setOnItemSelectedListener(
+                                new AdapterView.OnItemSelectedListener() {
+                                    @Override
+                                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                        // STORE INFORMATION
+                                        selectedName = (String) parent.getItemAtPosition(position);
+                                        if (selectedName == "None") {
+                                            selectedName = "Employee Name";
+                                            selectedID = "Employee ID";
+                                            selectedAge = "Employee Age";
+                                            selectedMail = "Employee Email";
+                                            selectedNum = "Employee Phone Number";
+                                        }
+                                        else {
+                                            selectedID = IDs.get(position);
+                                            DataSnapshot employee = dataSnapshot.child(selectedID);
+                                            selectedAge = employee.child("Age").getValue().toString();
+                                            selectedMail = employee.child("Email").getValue().toString();
+                                            selectedNum = employee.child("Phone Number").getValue().toString();
+                                        }
+
+                                        //Change Display Information
+                                        employeeID.setText(selectedID);
+                                        employeeName.setText(selectedName);
+                                        employeeAge.setText(selectedAge);
+                                        employeeMail.setText(selectedMail);
+                                        employeeNum.setText(selectedNum);
+
+                                    }
+
+                                    @Override
+                                    public void onNothingSelected(AdapterView<?> parent) {
+
+                                    }
+                                }
+                        );
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                }
+        );
     }
 
 
